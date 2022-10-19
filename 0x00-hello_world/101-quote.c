f9139954a0271cf2e4986699c4f4148f49ccd33c
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-19 03:34:26 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,15 +7,14 @@
 #include <unistd.h>
 
 /**
- * main - Print out "and that piece of art is useful
- * 			- Dora Korpar, 2015-10-19"
- * Return: Always 1 
- */
+* main - Print out "and that piece of art is useful
+*			- Dora Korpar, 2015-10-19"
+* Return: Always 1
+*/
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 				59);
-	
 		return (1);
 }
 

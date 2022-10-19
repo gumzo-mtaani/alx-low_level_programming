a96ee98af857c5b64b15b1c2866350bd096f4798
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-19 02:39:22 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+/*
+ * File: 4-puts.c
+ */
+
+#include <stdio.h>
+
+/*
+ * Prints "Programming is like building a multilingual puzzle"
+ * followed by a new line
+ * Return (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+
+
+}

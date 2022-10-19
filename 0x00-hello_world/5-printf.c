3419a105022cce375741677e2b0955ab721237a6
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-19 02:55:23 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-printf.c
+ * Auth: John M. Ngatia
+ */
+
+#include <stdio>
+
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art",
+ * 			followed by a new line
+ *
+ * return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+
+}

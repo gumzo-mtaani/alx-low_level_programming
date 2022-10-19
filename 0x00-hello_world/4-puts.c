ab7806a426cd7c1208f274b155e3b1ccb64991cf
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-19 02:47:41 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 /*
  * File: 4-puts.c
+ * Auth: John M. Ngatia <mugo.ngatia@gmail.com>
  */
 
 #include <stdio.h>
 
-/*
- * main - Prints "Programming is like building a multilingual puzzle"
- * followed by a new line
+/**
+ * main - Prints "Programming is like building a multilingual puzzle",
+ *		 followed by a new line
  * Return (0)
  */
 

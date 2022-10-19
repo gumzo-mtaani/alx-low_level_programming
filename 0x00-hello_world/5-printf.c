d6f1677a3ef0e6f9c39066ab73044d9e5bd895fa
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-19 03:07:09 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,21 @@
 /*
- * File: 5-printf.c
- * Auth: John Mugo Ngatia
+ *  File: 5-printf.c
+ *  Auth: John M. Ngatia
  */
 
 #include <stdio.h>
 
-
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art",
- * 	followed by a new line
- *
- * return: Always 0
- */
+ * main - Prints "with proper grammar, but the outcome is
+ *                   a piece of art," followed by a new line.
+ *    
+ * Return: Always 0.
+ * /
+
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
-
-
+		return (0);
 }

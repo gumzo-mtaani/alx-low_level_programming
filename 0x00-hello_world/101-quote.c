32d8ab6cd43c351eaa8c65d0c7a5eff4357c3cc3
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-19 03:31:19 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,11 +9,12 @@
 /**
  * main - Print out "and that piece of art is useful
  * 			- Dora Korpar, 2015-10-19"
- * Return: 1 if success.
+ * Return: Always 1 
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+				59);
 	
 		return (1);
 }

a01f766a51346ba90da03eb689a138e61391105f
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-25 18:38:21 +0000

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that writes alphabet in uper and lower case
+ *	and leaves starts a new line
+ *
+ * Return: 0 on success
+ *
+ * Auth: John M. Ngatia
+ * Filename: 3-print_alphabets.c
+ */
+
+int main(void)
+{
+
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+
+
+}

b05b85981ab0b0412f7fdc8a3ce27168b7a6a29f
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-26 03:12:07 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Lists all single digits in base ten starting from zero
+ * using putchar
+ *
+ * Return: 0 on success
+ *
+ * Auth: John M. Ngatia
+ * Filename: 6-print_numberz.c
+ */
+
+int main(void)
+
+{
+
+	int num;
+
+	for (num >= 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}

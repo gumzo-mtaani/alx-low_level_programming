8de6590cdb9340036e67604a3ad2b792679f9bad
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-26 03:56:54 +0000

    numbers separated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print single digits separated by commas
+ *
+ * Return: 0 on success
+ *
+ * Auth: John M. Ngatia
+ * Filename: 9-print_comb.c
+ */
+
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		putchar(',');
+	}
+
+	putchar('\n');
+
+	return (0);
+
+
+}

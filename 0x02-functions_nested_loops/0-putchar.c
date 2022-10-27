a03bc877f6ac6c0ec662d20660f9e97c7505ee00
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 06:10:50 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+/**
+ * Auth: John M. Ngatia
+ * Filename: 0-putchar.c
+ */
+
+#include<main.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the word _putchar
+ *
+ * Return 0 on success
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
+}
+
+#include<stdio.h>
+
+

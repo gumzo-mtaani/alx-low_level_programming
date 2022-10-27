4a8629c9db581724d5211e42737aa26703dee3d0
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 07:12:04 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print lowercase alphabet
+ *
+ * Program prints alphabet using _putchar
+ *
+ *Return 0 on success
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+	return (0);
+}

7998918f46357b2bc225d3def08bb6fd5d29829e
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 10:26:17 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,16 @@
 /**
  * print_last_digit - print last digit of a number
  * @n: character to be analysed
- * Return: 0
+ * Return: last digit
  */
 
 int print_last_digit(int n)
 {
+	int x;
+	
 	if (n < 0)
 		n = -n;
-	return (n % 10);
+	x = n % 10;
+	_putchar(x + '0');	
+	return (x);
 }

01f71605807ebd30407a2871ce307066c16760da
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 12:37:28 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - print fibonacci
+ * Description: prints fibonacci numbers
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	long int a, b, c, d;
+
+	a = 1;
+	b = 2;
+	c = 3;
+
+	printf("%ld, ", a);
+	printf("%ld, ", b);
+	for (d = 3; d < 50; d++)
+	{
+		printf("%ld, ", c);
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	printf("%ld\n", c);
+	return (0);
+}

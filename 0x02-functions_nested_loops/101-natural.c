bf7759d40bbbf0ccc8e0f871958ea2ca7200b0bf
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 12:27:47 +0000

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - natural numbers multiples of 3 and 5 until 1024
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+		int s, t, f, tm, fm;
+
+		s = 0;
+		for (t = 0; t <= (1024 / 3); t++)
+		{
+			tm = 3 * t;
+			s = s + tm;
+		}
+		for (f = +; f <= (1024 / 5); f++)
+		{
+			if (!(f % 3 == 0))
+			{
+				fm = 5 * f;
+				s = s + fm;
+			}
+		}
+		printf("%i\n", s);
+		return (0);
+}

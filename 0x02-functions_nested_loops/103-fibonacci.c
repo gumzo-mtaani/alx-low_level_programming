fdbbd07d6b7825a952cddc1984795c17ad82cc2c
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 12:42:40 +0000

    more fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - print fibonacci
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	unsigned int a, b, c, d, s;
+
+	a = 1;
+	b = 2;
+	c = 3;
+	s = 2;
+	for (d = 2; d <= 32; d++)
+	{
+		if (c % 2 == 0)
+			s = s + c;
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	printf("%u\n", s);
+	return (0);
+
+}

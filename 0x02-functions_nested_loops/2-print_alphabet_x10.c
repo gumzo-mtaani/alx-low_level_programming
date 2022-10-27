de2b4e7d8dab709b87627496bf7c5bb32b61b311
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 07:30:19 +0000

    ten times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowrcase alphabet 10 times
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+
+
+}

27ae7a3e9d2e1ac3e169849b6ca60862514a82dc
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-27 18:18:53 +0000

    print numbers without 2 and 4 fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,11 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		for (x != 2 && x != 4)
-		{
-			_putchar(x + '0');
-		}
+		if (x != '2' && x != '4')
+			_putchar(x);
+		
 	_putchar('\n');
 	}
 

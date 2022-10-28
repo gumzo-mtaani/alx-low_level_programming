566f6b86afc564fdb9ad8d2c8ba21517a4333102
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-28 13:27:27 +0000

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal
+ * @n: character to be input for diagonals
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+
+	if (x <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+			_putchar(95);
+		_putchar('\n');
+	}
+
+
+
+}

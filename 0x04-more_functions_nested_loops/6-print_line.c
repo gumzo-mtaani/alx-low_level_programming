1c345399b12945c8bf722f358a0704c9eba7318c
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-28 03:09:44 +0000

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - prints line based on provided value n
+ * @n: value provided
+ */
+
+void print_line(int n)
+{
+
+	int n;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('_');
+	}
+
+
+
+}

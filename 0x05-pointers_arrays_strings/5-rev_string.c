84c7664e952e1e97f54813dcf681e35dd758a46a
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 04:59:19 +0000

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,24 +7,24 @@
  */
 void rev_string(char *s)
 {
-	int i, j, temp, length;
+	int i, j, temp, len;
 
-	while (s[i] !=0)
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 	
-	length = i;
-	i = 0;
-	j = length -1;
+	len = i;
+	j = 0;
+	i--;
 	
-	while (i < j)
+	while (j < len / 2)
 	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		i--;
+		j++;
 	
 	}
 }

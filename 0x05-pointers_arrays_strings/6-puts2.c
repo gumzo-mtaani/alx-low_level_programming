913a1092d4173d241a50053cf6a8e3b4cf07fc82
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 05:16:38 +0000

    every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,17 +7,16 @@
 
 void puts2(char *str)
 {
-	int i;
-	int j;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0' ; i++)
+	while (*(str + i) != '\0')
 	{
-		;
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i++;
 	}
-	for (j = 0; j < i; j = j + 2)
-	{
-		_putchar(str[j]);
-	}
-	_putchar(str[j]);
+	_putchar('\n');
 
 }

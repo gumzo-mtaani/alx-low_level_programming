78351cc41f9b9e4bd76ec723e41f043bb51b77a8
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 04:54:11 +0000

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ *
+ */
+void rev_string(char *s)
+{
+	int i, j, temp, length;
+
+	while (s[i] !=0)
+	{
+		i++;
+	}
+	
+	length = i;
+	i = 0;
+	j = length -1;
+	
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	
+	}
+}

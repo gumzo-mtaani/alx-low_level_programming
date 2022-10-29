ffca666aac7194ee178a21dbd5704177ba56b95b
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 06:03:47 +0000

    arrays not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,15 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
+	
+	i = 0;
+	while ( i < n)
 	{
-		printf("%d", *(a + i));
-		if (i < n - 1)
-			printf(", ");
+		if (i != n -1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
 	}
 	_putchar('\n');
 }

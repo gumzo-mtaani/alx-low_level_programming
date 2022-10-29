39132ef12412186986a437d9b69d28dde30d4c0d
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 15:02:39 +0000

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,69 +1,65 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: 1st number
- * @n2: 2nd number
- * @r: pointer to the buffer to store result
- * @size_r: size of the buffer
- * Return: 0 if buffer is too small to store result, else return buffer
- * to pointer
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+	i++;
+	return (i);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to reverse
+ * Return: void
+ */
+char *rev_string(char *s)
+{
+	int l = _strlen(s), i = 0;
+	char t;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		t = s[l - i - 1];
+		s[l - i - 1] = s[i];
+		s[i] = t;
+	}
+	return (s);
+}
+
+/**
+ * infinite_add - adds arbitrarily long string of digits
+ * @n1: the first digit string
+ * @n2: the second digit string
+ * @r: the result buffer
+ * @size_r: the size of result buffer
+ * Return: char pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int 11, 12, tmp1, r1, i, sum, num1, num2, carry;
-	char tmp[10000];
+	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
 
-	r1 = i = 11 = 12 = sum = num1 = num2 = carry = 0;
-	while (n1[11] != '\0')
-		11++;
-	while (n2[12] != '\0')
-		12++;
-	if (11 + 2 > size_r || 12 + 2 > size_r)
-		return (0);
-	11--;
-	12--;
-	while (i <= 11 || i <= 12)
+	for (l1--, l2--, size_r--; l1 >= 0 || l2 >= 0 || c; l1--, l2--, i++)
 	{
-		num1 = num2 = 0;
-		if (i <= 11)
-			num1 = n1[11 - i] - '0';
-		if (i <=12 && (12 -i) >= 0)
-			num2 = n2[12 - i] - '0';
-		sum = num1 + num2 + carry;
-		if (sum >= 10)
-		{
-			carry = 1;
-			sum -= 10;
-		}
-		else
-			carry = 0;
-		r[i] = sum + '0';
-		i++;
-		r1++;
+		if (i >= size_r)
+			return (0);
+		a = 0;
+		b = 0;
+		if (l1 >= 0)
+			a = n1[l1] - '0';
+		if (l2 >= 0)
+			b = n2[l2] - '0';
+		a = a + b + c;
+		c = a / 10;
+		a %= 10;
+		r[i] = a + '0';
 	}
-	if (carry > 0)
-	{
-		r[i] = carry + '0';
-		r[i + 1] = '\0';
-	}
-	i = tmp1 = 0;
-	while (i <= r1)
-	{
-		tmp[i] = r[r1 -i];
-		tmp1++;
-		i++;
-	}
-	i = 0;
-	while (i < tmp1)
-	{
-		if (r[i] == '\0')
-		{
-			break;
-		}
-		r[i] = tmp[i];
-		i++;
-	}
-	return (r);
-
+	r[i] = '\0';
+	return (rev_string(r));
 }

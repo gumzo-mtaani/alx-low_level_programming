a2a353a109b22d8cd43e5ee523618986d3b8787f
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 04:21:34 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the
+ * value to 98
+ * @n: character to be analyzed
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}

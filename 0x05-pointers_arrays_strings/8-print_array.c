85d06aa1810ded00487b33c2b69b4ca43890f5f8
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 05:54:50 +0000

    arrays not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * print_array - print n times elements of an array
+ * @a: pointer
+ * @n: times to print elements
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	_putchar('\n');
+}

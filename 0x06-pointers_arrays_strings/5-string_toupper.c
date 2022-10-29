dc621c036d907bd751ec412e78b7a24895148234
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-10-29 12:59:41 +0000

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string to change
+ * Return: changed string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i} = str[i] - 32;
+		}
+		i++
+	}
+	return (str);
+}

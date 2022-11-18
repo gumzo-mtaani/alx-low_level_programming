4295756b5cc6b2c10892a3fe10a7d9cf2c60ce29
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-11-18 16:21:03 +0000

    diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,21 @@
 
 void print_diagsums(int *a, int size)
 {
-	int b, sum1 = 0, sum2 = 0;
+	int i = 0;
+	int j = size - 1;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	for (b = 0; b < size; b++)
+	while (i <= (size * size))
 	{
-		sum1 += a[(size + 1) * b];
-		sum2 += a[(size - 1) * (b + 1)];
+		sum1 = sum1 + a[i];
+		i = i + size + 1;
+	}
+
+	while (j < (size * size - 1))
+	{
+		sum2 += a[j];
+		j = j + size - 1;
 	}
 
 	printf("%d, %d\n", sum1, sum2);

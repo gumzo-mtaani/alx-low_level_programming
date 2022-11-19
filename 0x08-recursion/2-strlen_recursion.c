6c49a4de6ce0860d6ed8fa102f9e321407237d36
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-11-19 08:34:08 +0000

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to return length of a string
+ * @s: variable input
+ * Return: 0 on success
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\n')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}

6139bb6f1b21407f8342830403afbe5fea05f0cf
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-11-19 08:11:24 +0000

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - the function
+ * @s: variable input
+ * Return: 0 on success
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion (s + 1);
+	else
+		return;
+	_putchar(*s);
+}

2483a219b6b8e0d1cae20ec41e9e197bcb92bf06
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-12-09 13:59:19 +0000

    printme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main . function to print file name
+ * @argc: argument count
+ * @argv: array of pointers to strings
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if (arg > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}

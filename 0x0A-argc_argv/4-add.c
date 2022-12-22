822b34ea1844d52315ec81b2af052344a467f331
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-12-22 18:09:03 +0000

    add numbers errors corrected

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,31 +12,27 @@
 int main(int argc, char *argv[])
 {
 
-	int result, num, i, j, k;
-
-		if (argc == 1)
-			printf("0\n");
+	int result = 0, num, i, j, k;
 
 
-		for (i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				if (argv[i][j] > '9' || argv[i][j] < '0')
-				{
-					printf("%s\n", "Error");
-					return (1);
-				}
+				printf("%s\n", "Error");
+				return (1);
 			}
 		}
-		for (k = 1; k < argc; k++)
-		{
-			num = atoi(argv[k]);
-			result += num;
-		}
-
-		printf("%d\n", num);
-		return (0);
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", num);
+	return (0);
 
 
 }

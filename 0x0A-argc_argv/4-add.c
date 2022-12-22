031570c5ccb4a4c68cbef92ca20177bf69862803
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-12-22 17:56:15 +0000

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argc parameter
+ * @argv: array of commands listed
+ * Return: 0 on success 1 on fail
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	int result = 0, num, i, j, k;
+
+		if (argc == 1)
+			printf("0\n");
+
+
+		for (i = 0; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+		for (k = 0; k < argc; k++)
+		{
+			num = atoi(argv[k]);
+			result += num;
+		}
+
+		printf("%d\n", num);
+		return (0);
+
+
+}

78aa43defb23d32f518ea5e5a812c01a0ce99907
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-12-22 16:41:29 +0000

    changes made

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints arguments passed
+ * @argc: an array
+ * @argv: an array of a command listed
+ * Return: 0 on success
+ */
+
+
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	//Print the number of arguments passed to the program

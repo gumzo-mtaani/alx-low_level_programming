c9c638ceb502c626ae4a86829ae8dc563b7d3d25
gumzo-mtaani <mugo.ngatia@gmail.com>
2022-12-22 16:25:34 +0000

    corrections

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include "main.h"
+
 
 int main(int argc, char *argv[])
 {
 	//Print the number of arguments passed to the program
-	printf("%d\n", argc - 1);
+	int i;
 
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+		for (i = 0; *argv; i++; argv++);
+		printf("%d\n", i - 1);
 	return 0;
 
 }

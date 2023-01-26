7e412ff4ccd92d1e002716a6bcfcf9f8c32540c4
gumzo-mtaani <mugo.ngatia@gmail.com>
2023-01-26 14:08:50 +0300

    errors corrected and checked

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * create_array - creates an array and starts with special character
  * @size: array size
  * @c: special character
  * Return: pointer to array or NULL on fail
@@ -11,25 +10,30 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
 
-	
 	if (size == 0)
-	{
-		return (NULL);	
-	}
-	
-	char *array = (char *) malloc(sizeof(char) * size);
-	
-	if (array == NULL)
 	{
 		return (NULL);
 	}
-	
-	for (unsigned int i = 0; i < size; i++)
-	{
-		array[i] = c;
-	}
-	
-	return (array);
 
+	else
+	{
+		array = malloc(sizeof(char) * size);
+
+		if (array == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			while (i < size)
+			{
+				array[i] = c;
+				i++;
+			}
+		}
+	return (array);
+	}
 }

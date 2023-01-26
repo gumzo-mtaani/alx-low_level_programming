278009c31cb95cd524087466cbdd99e405684734
gumzo-mtaani <mugo.ngatia@gmail.com>
2023-01-26 19:10:12 +0300

    errors corrected

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,9 +15,11 @@ typedef struct dog
 	char *owner;
 } dog;
 
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 int _putchar(char c);
 
 #endif
